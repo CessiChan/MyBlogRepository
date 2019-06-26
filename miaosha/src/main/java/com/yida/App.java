02c76e101e74fa2fa0c3426ac071367685dacf4e
CessiChan <si_chan@163.com>
2019-06-26 15:37:21 +0800

    springBoot和mybatis generator搭建完成

--- miaosha/src/main/java/com/yida/App.java
+++ miaosha/src/main/java/com/yida/App.java
@@ -1,7 +1,11 @@
 package com.yida;
 
+import com.yida.dao.UserInfoDOMapper;
+import com.yida.dataObject.UserInfoDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,17 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  */
 //此注解为了自动加载,启动内嵌tomcat
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.yida"})
 @RestController
+@MapperScan("com.yida.dao")
 public class App {
 
+    @Autowired
+    private UserInfoDOMapper userInfoDOMapper;
+
     @RequestMapping("/")
-    public String home(){
-        return "HelloWorld!";
+    public String home() {
+        UserInfoDO userInfoDO=userInfoDOMapper.selectByPrimaryKey("521aff9397e311e9a80d00ff4fdfe997");
+        if(userInfoDO!=null) {
+            return userInfoDO.getName();
+        }else{
+            return "用户信息不存在";
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        SpringApplication.run(App.class,args);
+        SpringApplication.run(App.class, args);
     }
 }
